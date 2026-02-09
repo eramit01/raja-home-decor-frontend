73e5a9f4c5570e1f38c4fad937744cf526790517
unknown <ptamit7826@gmail.com>
2026-02-09 18:11:09 +0530

    Add Redux persist for authentication state

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { BrowserRouter } from 'react-router-dom';
 import { Toaster } from 'react-hot-toast';
 import App from './App';
-import { store } from './store';
+import { store, persistor } from './store';
 import { WishlistProvider } from './context/WishlistContext';
 import { LoginModal } from './components/LoginModal';
 import './index.css';
@@ -25,15 +26,17 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <HelmetProvider>
       <Provider store={store}>
-        <WishlistProvider>
-          <QueryClientProvider client={queryClient}>
-            <BrowserRouter future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
-              <App />
-              <Toaster position="top-center" />
-              <LoginModal />
-            </BrowserRouter>
-          </QueryClientProvider>
-        </WishlistProvider>
+        <PersistGate loading={null} persistor={persistor}>
+          <WishlistProvider>
+            <QueryClientProvider client={queryClient}>
+              <BrowserRouter future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
+                <App />
+                <Toaster position="top-center" />
+                <LoginModal />
+              </BrowserRouter>
+            </QueryClientProvider>
+          </WishlistProvider>
+        </PersistGate>
       </Provider>
     </HelmetProvider>
   </React.StrictMode>
